16240563d00be65c1afd581a967130ec880a7774
vsha041 <varun.varunsharma@outlook.co.nz>
2026-07-26 17:53:38 +1200

    initial code to test out reflection stuff

---
+++ ReflectionTest/ReflectionTest/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ReflectionTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var metadata = new Model
+            {
+                Name = "cow"
+            };
+
+            //var metadata2 = new List<Model>();
+            //metadata2.Add(new Model()
+            //{
+            //    Name = "one"
+            //});
+            //metadata2.Add(new Model()
+            //{
+            //    Name = "two"
+            //});
+            var properties = ReflectionHelper.HeaderProperties.GetOrAdd(
+                metadata.GetType(),
+                ReflectionHelper.FindHeaderProperties);
+
+            foreach (var property in properties)
+            {
+                var value = property.Property.GetValue(metadata);
+                if (value is not null)
+                {
+                    var result = ReflectionHelper.FormatHeaderValue(value);
+                    Console.WriteLine($"{property.HeaderName} - {result}");
+                }
+            }
+        }
+    }
+
+    public class Model
+    {
+        [AddApiResponseHeader("crocodile")]
+        public string? Name { get; set; }
+    }
+}
---
+++ ReflectionTest/ReflectionTest/ReflectionHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+using System.Globalization;
+using System.Reflection;
+
+namespace ReflectionTest;
+
+public class ReflectionHelper
+{
+    public static readonly ConcurrentDictionary<Type, HeaderProperty[]> HeaderProperties = new();
+
+    public static HeaderProperty[] FindHeaderProperties(Type metadataType) =>
+        metadataType
+            .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+            .Select(property => new
+            {
+                Property = property,
+                Attribute = property.GetCustomAttribute<AddApiResponseHeaderAttribute>(
+                    inherit: true)
+            })
+            .Where(item => item.Attribute is not null && item.Property.CanRead)
+            .Select(item => new HeaderProperty(item.Attribute!.Name, item.Property))
+            .ToArray();
+
+    public static string FormatHeaderValue(object value) => value switch
+    {
+        Guid guid => guid.ToString("D"),
+        DateTime dateTime => dateTime.ToString("O", CultureInfo.InvariantCulture),
+        DateTimeOffset dateTimeOffset =>
+            dateTimeOffset.ToString("O", CultureInfo.InvariantCulture),
+        IFormattable formattable =>
+            formattable.ToString(format: null, CultureInfo.InvariantCulture),
+        _ => value.ToString() ?? string.Empty
+    };
+
+    public sealed record HeaderProperty(string HeaderName, PropertyInfo Property);
+}
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public sealed class AddApiResponseHeaderAttribute : Attribute
+{
+    public AddApiResponseHeaderAttribute(string name)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        Name = name;
+    }
+
+    public string Name { get; }
+}
